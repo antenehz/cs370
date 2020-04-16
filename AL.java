99187616a0a323945d17b5bc38300a3593ef78b4
vanessajhughes <62395064+vanessajhughes@users.noreply.github.com>
2020-04-16 17:56:01 -0600

    Add files via upload

---
+++ AL.java
@@ -0,0 +1,45 @@
+import java.awt.*;
+import java.awt.event.*;
+
+public class AL extends Frame implements WindowListener,ActionListener {
+        TextField text = new TextField(20);
+        Button b;
+        private int numClicks = 0;
+
+        public static void main(String[] args) {
+                AL myWindow = new AL("My first window");
+                myWindow.setSize(350,100);
+                myWindow.setVisible(true);
+        }
+
+        public AL(String title) {
+
+                super(title);
+                setLayout(new FlowLayout());
+                addWindowListener(this);
+                b = new Button("Click me");
+                add(b);
+                //add(text);
+                b.addActionListener(this);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+		try{
+                Runtime rt = Runtime.getRuntime();
+		Process pr = rt.exec("fswebcam image.jpg");}
+		catch (Exception error){}
+ 	}
+
+        public void windowClosing(WindowEvent e) {
+                dispose();
+                System.exit(0);
+        }
+
+        public void windowOpened(WindowEvent e) {}
+        public void windowActivated(WindowEvent e) {}
+        public void windowIconified(WindowEvent e) {}
+        public void windowDeiconified(WindowEvent e) {}
+        public void windowDeactivated(WindowEvent e) {}
+        public void windowClosed(WindowEvent e) {}
+
+}
