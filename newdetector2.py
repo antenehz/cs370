89aa4d7a46c3919574c32dbca9b1074792ccc3ca
vanessajhughes <62395064+vanessajhughes@users.noreply.github.com>
2020-04-30 15:07:30 -0600

    Add files via upload

--- newdetector2.py
+++ newdetector2.py
@@ -1,4 +1,5 @@
 import boto3
+import os
 
 #create files here
 
@@ -10,19 +11,21 @@ def detect_emotions(photo):
         	response = client.detect_faces(Image={'Bytes': image.read()}, Attributes = ['ALL'])
 
 	con = 0
-	emo = {}
+	emo = ''
 	for face in response['FaceDetails']:
 		for emotion in face['Emotions']:
 			if(con < emotion['Confidence']):
 				con = emotion['Confidence']
 				if(emotion['Type'] == 'HAPPY' or emotion['Type'] == 'SAD'):
 					emo = emotion['Type']
-
+	if emo =='':
+		emo = 'HAPPY'
 	f = open("emotion.txt", "w+")
 	f.write(emo)		
 def main():
-	photo = '/home/pi/pi-detector/faces/smile.jpeg'
+	photo = '/home/pi/pi-detector/faces/image.jpg'
 	detect_emotions(photo)
+	os.system("python userTopTracks1.py hi")
 
 if __name__ == '__main__':
     main()
