76ca554794f53f550b4a29bdb0f50d792608d3b6
vanessajhughes <62395064+vanessajhughes@users.noreply.github.com>
2020-04-30 10:48:26 -0600

    Add files via upload
    updated new detector

--- newdetector2.py
+++ newdetector2.py
@@ -4,20 +4,27 @@ import boto3
 
 
 def detect_emotions(photo):
-	client = boto3.client('rekognitions')
+	client = boto3.client('rekognition')
 
 	with open(photo, 'rb') as image:
         	response = client.detect_faces(Image={'Bytes': image.read()}, Attributes = ['ALL'])
-	
-	for emotion in response['Emotion']:
-		#Maddies code goes here that writes emotions to files
-	
 
+	con = 0
+	emo = {}
+	for face in response['FaceDetails']:
+		for emotion in face['Emotions']:
+			if(con < emotion['Confidence']):
+				con = emotion['Confidence']
+				if(emotion['Type'] == 'HAPPY' or emotion['Type'] == 'SAD'):
+					emo = emotion['Type']
+			#Maddies code goes here that writes emotions to files
+			#print"  {Type} : {Confidence}%".format(**emotion)
 
+	f = open("emotion.txt", "w+")
+	f.write(emo)		
 def main():
-	photo = '/home/pi/pi-detector/faces/image.jpg'
-	response = detect_emotions(photo)
-	print(response)
+	photo = '/home/pi/pi-detector/faces/smile.jpeg'
+	detect_emotions(photo)
 
 if __name__ == '__main__':
     main()
